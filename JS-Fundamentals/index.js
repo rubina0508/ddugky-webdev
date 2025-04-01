fac26757daa43d56a4e823dcb58ea8adc93ac28b
Rubina Ahirwar <rubina.a.0508@ gmail.com>
2025-03-31 22:50:18 -0700

    data types and variable

---
+++ JS-Fundamentals/index.js
@@ -0,0 +1,35 @@
+//////////////////////////////
+// 01. DATA TYPES
+//////////////////////////////
+
+// 1. String , "rubina", "19", "true"
+// 2. Numbar ,26
+// 3. Bigint
+// 4. Boolean , true / false
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+
+
+///////////////////////////
+// 02. Variables
+//////////////////////////
+
+// Identifiers/name & Literels/Value
+var firstname = "rubina";
+var lastname = "ahirwar";
+var age = 19;
+var married = true;
+var husbandname = "siddhant";
+var thirdname  = null
+
+console.log (firstname, lastname);
+//print : "good Mornig Rubina Ahirwar !"
+console.log("good Mornig", firstname, lastname,"!");
+console.log("Hi, My name is", firstname, "and my age is", age, ".")
+console.log("husbandname")
+
+
+
+
+
